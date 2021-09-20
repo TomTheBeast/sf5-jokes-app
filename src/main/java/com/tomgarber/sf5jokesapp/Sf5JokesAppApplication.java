7080f7bad14ac44962c7cf3be8f11c81d880ed67
Tom Garber <thogarbe@amazon.com>
2021-09-20 10:50:12 -0500

    Create Project

---
+++ src/main/java/com/tomgarber/sf5jokesapp/Sf5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.tomgarber.sf5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf5JokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomgarber/sf5jokesapp/Sf5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomgarber.sf5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf5JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
